47643b4bb7fe063a1fa3a256c98c4f67e6148cb6
Ricas78 <ricardo.dias78@hotmail.com>
2024-10-25 18:37:59 -0300

    stls add e batch

--- Altprint/batch_flex.py
+++ Altprint/batch_flex.py
@@ -14,7 +14,7 @@ os.chdir("D:/códigosVScode/continuous_altprint/Altprint")
 
 process1 = FlexProcess(settings_file='flex_bar.yml')
 process2 = FlexProcess(settings_file='flex_bar2.yml')
-process3 = FlexProcess(settings_file='flex_bar3.yml')
+# process3 = FlexProcess(settings_file='flex_bar3.yml')
 
 part1 = FlexPrint(process1)
 part1.slice()
@@ -26,13 +26,13 @@ part2.slice()
 part2.make_layers()
 part2.export_gcode("flex_bar.gcode")
 
-part3 = FlexPrint(process3)
-part3.slice()
-part3.make_layers()
-part3.export_gcode("flex_bar.gcode")
+# part3 = FlexPrint(process3)
+# part3.slice()
+# part3.make_layers()
+# part3.export_gcode("flex_bar.gcode")
 
 multi_process = MultiProcess(
-    parts=[part1, part2, part3],
+    parts=[part1, part2],
     start_script="scripts/start_gtmax.gcode",
     end_script="scripts/end_gtmax.gcode")
 
